31501b8437cd3a725c3c484d366def692c5108f6
KALEEM IQBAL <olduser0071@gmail.com>
2024-09-07 21:28:51 +0530

    codes

---
+++ SquareBetweenTwoNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace SquareBetweenTwoNumbers;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter Lower Bound");
+        int lowerBound = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter Upper Bound");
+        int upperBound = Convert.ToInt32(Console.ReadLine());
+
+        int start = (int)Math.Ceiling(Math.Sqrt(lowerBound));
+        int end = (int)Math.Floor(Math.Sqrt(upperBound));
+  
+        for (int i = start; i <= end; i++)
+            Console.WriteLine($"Squre of {i} is {i * i}");
+
+        Console.ReadLine();
+    }
+}
